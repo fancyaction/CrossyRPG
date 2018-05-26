55e57c712c6929d0b182231b970029e2738f4132
fancyaction <labcoatdisaster@gmail.com>
2018-05-26 13:46:09 -0500

    Complete

--- src/index.js
+++ src/index.js
@@ -13,6 +13,40 @@ let config = {
 // create game with config
 let game = new Phaser.Game(config);
 
+// scene parameters (custom -- not part of Phaser API)
+gameScene.init = function() {
+  this.playerSpeed = 1.5;
+  this.enemyMaxY = 280;
+  this.enemyMinY = 80;
+
+  //end the game
+  gameScene.gameOver = function() {
+    // set player to dead
+    this.isPlayerAlive = false;
+
+    this.cameras.main.shake(500);
+
+    // fade camera
+    this.time.delayedCall(
+      250,
+      () => {
+        this.cameras.main.fade(250);
+      },
+      [],
+      this
+    );
+    // restart game
+    this.time.delayedCall(
+      500,
+      () => {
+        this.scene.restart();
+      },
+      [],
+      this
+    );
+  };
+};
+
 // load assets
 gameScene.preload = function() {
   this.load.image('bg', 'src/assets/images/background.png');
@@ -27,5 +61,86 @@ gameScene.create = function() {
 
   this.player = this.add
     .sprite(40, this.sys.game.config.height / 2, 'player')
-    .setScale(0.5);
+    .setScale(0.4);
+
+  this.isPlayerAlive = true;
+
+  this.treasure = this.add
+    .sprite(
+      this.sys.game.config.width - 80,
+      this.sys.game.config.height / 2,
+      'treasure'
+    )
+    .setScale(0.6);
+
+  this.enemies = this.add.group({
+    key: 'dragon',
+    repeat: 5,
+    setXY: {
+      x: 110,
+      y: 100,
+      stepX: 80,
+      stepY: 20
+    }
+  });
+  Phaser.Actions.ScaleXY(this.enemies.getChildren(), -0.5, -0.5);
+  Phaser.Actions.Call(
+    this.enemies.getChildren(),
+    enemy => {
+      enemy.speed = Math.random() * 1 + 0.5;
+    },
+    this
+  );
+
+  // reset camera effects
+  this.cameras.main.resetFX();
+};
+
+gameScene.update = function() {
+  if (!this.isPlayerAlive) {
+    return;
+  }
+
+  // player movement
+  // if user clicks/touches screen ...
+  if (this.input.activePointer.isDown) {
+    // player walks
+    this.player.x += this.playerSpeed;
+  }
+
+  //   treasure collision
+  if (
+    Phaser.Geom.Intersects.RectangleToRectangle(
+      this.player.getBounds(),
+      this.treasure.getBounds()
+    )
+  ) {
+    this.gameOver();
+  }
+
+  // enemy movement
+  let enemies = this.enemies.getChildren();
+
+  for (let i = 0; i < enemies.length; i++) {
+    // move enemies
+    enemies[i].y += enemies[i].speed;
+
+    // reverse movement when edge is reached
+    if (enemies[i].y >= this.enemyMaxY && enemies[i].speed > 0) {
+      enemies[i].speed *= -1;
+    } else if (enemies[i].y <= this.enemyMinY && enemies[i].speed < 0) {
+      enemies[i].speed *= -1;
+    }
+
+    // enemy collision
+    if (
+      Phaser.Geom.Intersects.RectangleToRectangle(
+        this.player.getBounds(),
+        enemies[i].getBounds()
+      )
+    ) {
+      this.gameOver();
+      break;
+    }
+  }
 };
